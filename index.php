7f480f0c3ef586f6989aff53a34bbfc1be9c364d
gustavooromao <gustavo.elastico@estudante.ifto.edu.br>
2026-01-29 22:09:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-3">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha primeira página</title>
+    <link rel="styleheet" href="assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Minha primeira página</h1>
+        <?php
+        $mensagemBoasVindas = "Seja bem-vindo ao meu site";
+        $nome = "Gustavo";
+        echo <h1>$mensagemBoasVindas <br> Meu nome é $nome </h1> #br para quebra de página
+        ?>
+    </div>
+  
+</body>
+</html>
